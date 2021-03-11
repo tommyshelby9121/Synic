9cbdc9bea2a681f5485540e28351e6b58e4472a9
Sycrixx <biometricmc@gmail.com>
2021-03-12 00:00:23 +0500

    Create simpleEmbed

--- bot/src/utils/embedBuilder.ts
+++ bot/src/utils/embedBuilder.ts
@@ -9,3 +9,12 @@ export async function permissionEmbed(message:Message, description:string, color
 
     await message.channel.send(embed);
 }
+
+export async function simpleEmbed(message:Message, description:string, color:string) {
+    const embed = new MessageEmbed()
+        .setDescription(description)
+        .setColor(color)
+        .setFooter(`Requested By: ${message.author.tag} | Synic v1.0`);
+
+    await message.channel.send(embed);
+}
