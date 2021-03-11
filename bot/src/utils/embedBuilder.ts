6a569a4f1d1a834292b2598c61878f3aa2e855ea
Sycrixx <biometricmc@gmail.com>
2021-03-11 23:00:02 +0500

    Changed function name & added permission

--- bot/src/utils/embedBuilder.ts
+++ bot/src/utils/embedBuilder.ts
@@ -1,9 +1,10 @@
 import { Message, MessageEmbed } from "discord.js";
 
-export async function simpleEmbed(message:Message, description:string, color:string) {
+export async function permissionEmbed(message:Message, description:string, color:string, permission:string) {
     const embed = new MessageEmbed()
         .setDescription(description)
         .setColor(color)
+        .addField("Required Permission", permission)
         .setFooter(`Requested By: ${message.author.tag} | Synic v1.0`);
 
     await message.channel.send(embed);
