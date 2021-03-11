201e9433cc3725f11af4d2a31a5b8094de1b0700
Sycrixx <biometricmc@gmail.com>
2021-03-11 18:09:37 +0500

    Create avatar command

---
+++ bot/src/commands/utility/avatar.ts
@@ -0,0 +1,24 @@
+import { SynicClient } from "../../struct/Client";
+import { Message, MessageEmbed } from "discord.js";
+
+export default {
+    name: "avatar",
+    description: "Get the avatar of any user",
+    /**
+     * @param { SynicClient } client
+     * @param { Message } message
+     * @param { String } args
+     */
+    async execute(client:SynicClient, message:Message, args:ReadonlyArray<string>) {
+        let user = message.mentions.users.first() || client.users.cache.get(args[0]);
+        if (!user) user = message.author;
+
+        const avatarEmbed = new MessageEmbed()
+            .setTitle(`${user} avatar`)
+            .setColor("#FF7F50")
+            .setImage(user.displayAvatarURL({ dynamic: true, size: 256 }))
+            .setFooter(`Requested By: ${message.author.tag} | Synic v1.0`);
+
+        await message.channel.send(avatarEmbed);
+    }
+}
