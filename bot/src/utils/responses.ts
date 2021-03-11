411cf3e1ec45db9503dcae67cdf716e41b677fd4
Sycrixx <biometricmc@gmail.com>
2021-03-11 22:45:37 +0500

    Add global responses

---
+++ bot/src/utils/responses.ts
@@ -0,0 +1,8 @@
+export default {
+    global: {
+        no_user_permission: "You do not have the required permission to execute this command!",
+        no_bot_permission: "I am unable to execute this command. Please provide me with the required permission!",
+        no_user_provided: "Please specify a user",
+        no_case_arg: "Please provide a case number",
+    }
+}
