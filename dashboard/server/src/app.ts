277738f56b601acce22eaa712a072bf07642d548
Sycrixx <biometricmc@gmail.com>
2021-03-13 00:44:22 +0500

    Init connectDB

--- dashboard/server/src/app.ts
+++ dashboard/server/src/app.ts
@@ -3,9 +3,13 @@ config();
 import express, { Application } from "express";
 import logger from "morgan";
 import cookieParser from "cookie-parser";
+import connectDB from "./database/connection";
 
 // Init Express
 const app:Application = express();
+
+// Connect to MongoDB
+connectDB().catch(err => console.error(`Error establishing database connection: ${err.message}`));
 
 // Logging
 if (process.env.NODE_ENV === "development") {
