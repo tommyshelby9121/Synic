f0bc47879753cd10c7870fe1980e1f15da9f2a4a
Sycrixx <biometricmc@gmail.com>
2021-03-13 00:52:29 +0500

    Get things from profile & filter guilds

--- dashboard/server/src/config/passport.ts
+++ dashboard/server/src/config/passport.ts
@@ -7,6 +7,14 @@ module.exports = (passport:any) => {
         callbackURL: process.env.DISCORD_APP_CLIENT_REDIRECT!,
         scope: ["identify", "guilds"]
     }, async (accessToken:string, refreshToken:string, profile:Profile, done:any) => {
-
+        const { id, username, discriminator, avatar, guilds } = profile;
+        try {
+            // Filter user guilds and only include ones with permission 0x20
+            const filteredGuilds = guilds?.filter(object => (object.permissions & 0x20) === 0x20);
+        }
+        catch (err) {
+            console.error(`Error logging in user: ${err.message}`);
+            process.exit(1);
+        }
     }));
 }
