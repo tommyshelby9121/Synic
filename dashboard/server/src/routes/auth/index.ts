8fb2171bbe815735654ac63c14cc3a562aea2bc2
Sycrixx <biometricmc@gmail.com>
2021-03-13 23:25:15 +0500

    Add redirect

--- dashboard/server/src/routes/auth/index.ts
+++ dashboard/server/src/routes/auth/index.ts
@@ -1,9 +1,15 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import passport from "passport";
 const router = Router();
 
 // @route       GET /auth
 // @desc        Authenticate w/Discord
 router.get("/", passport.authenticate("discord"));
+
+// @route       GET /auth/redirect
+// @desc        Redirection Validation
+router.get("/redirect", passport.authenticate("discord"), (req:Request, res:Response) => {
+    res.redirect(`${process.env.CLIENT_SIDE_APP_URL}/account`);
+});
 
 export default router;
